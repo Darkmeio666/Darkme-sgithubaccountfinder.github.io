ac5d8478f6ef208d074705fe865aaff32bc25e23
DarkMe <123799633+Darkmeio666@users.noreply.github.com>
2023-04-15 15:00:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const searchUser = document.getElementById("searchUser");
+// Init github
+const github = new Github();
+// Init ui
+const ui = new UI();
+
+searchUser.addEventListener("keyup", (e) => {
+  const userText = e.target.value;
+  if (userText !== "") {
+    github.getUser(userText).then((data) => {
+      if (data.profile.message === "Not Found") {
+        // Show alert
+        ui.showAlert("User not found", "alert alert-danger");
+      } else {
+        //  Show Profile
+        ui.showProfile(data.profile);
+        ui.showrepos(data.repos);
+      }
+    });
+  } else {
+    // Clear profile
+    ui.clearProfile();
+  }
+});
